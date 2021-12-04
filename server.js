69d2bcd1c27652919ce265a0ea0eff8e7a67819a
Dr-Stasis <46714614+Dr-Stasis@users.noreply.github.com>
2021-12-04 17:55:44 -0500

    forgot to remove duplicate function

--- server.js
+++ server.js
@@ -29,14 +29,6 @@ app.get("/app/", (req, res, next) => {
 app.post("/app/new/", (req, res) => {
 	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)");
 	const info = stmt.run(req.body.user, md5(req.body.pass));
-	res.status(201).json({
-		message: `${info.changes} record created: ID ${req.params.id} (201)`
-	});
-});
-
-app.post("/app/new/", (req, res) => {
-	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)");
-	const info = stmt.run(req.body.user, md5(req.body.pass));
 	res.status(201).json({"message":info.changes+" record created: ID "+info.lastInsertRowid+ " (201)"    });
 });
 
